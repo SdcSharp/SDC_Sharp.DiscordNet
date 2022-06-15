b2a30894386256166922b3abe75b9f193316b828
Alexander <satan1c@ukr.net>
2022-06-15 11:29:34 +0300

    Removing unnecessary classes

--- Types/DiscordClientWrapperBase.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SDC_Sharp.DiscordNet.Types
-{
-    public abstract class DiscordClientWrapperBase : IDiscordClientWrapper
-    {
-        public abstract int ShardCount { get;  }
-        public abstract int ServersCount { get; }
-        public abstract ulong CurrentUserId { get; }
-    }
-}
--- Types/IDiscordClientWrapper.cs
+++
@@ -1,9 +0,0 @@
-using Discord.WebSocket;
-
-namespace SDC_Sharp.DiscordNet.Types
-{
-    public interface IDiscordClientWrapper : SDC_Sharp.Types.IDiscordClientWrapper
-    {
-        internal BaseSocketClient Client { get; }
-    }
-}
