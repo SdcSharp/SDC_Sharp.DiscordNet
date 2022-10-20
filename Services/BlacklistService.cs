75ecc08cea38dbc4b787946d18e0c3f6780a8287
Alexander <satan1c@ukr.net>
2022-10-20 15:08:01 +0300

    Reformatting

--- Services/BlacklistService.cs
+++ Services/BlacklistService.cs
@@ -4,21 +4,21 @@ using SDC_Sharp.Services;
 
 namespace SDC_Sharp.DiscordNet.Services
 {
-    public class BlacklistService : BaseBlacklistService
-    {
-        private readonly IClientConfig m_clientConfig;
+	public class BlacklistService : BaseBlacklistService
+	{
+		private readonly IClientConfig m_clientConfig;
 
-        public BlacklistService(SdcSharpClient client, SdcServices sdcServices) : base(client)
-        {
-            m_clientConfig = sdcServices.Client;
-        }
+		public BlacklistService(SdcSharpClient client, SdcServices sdcServices) : base(client)
+		{
+			m_clientConfig = sdcServices.Client;
+		}
 
-        public async Task<UserWarns> GetWarns(ulong userId, bool fetch = false)
-        {
-            var warns = await GetWarns<UserWarns>(userId);
-            if (fetch)
-                warns.User = await m_clientConfig.Rest.GetUserAsync(warns.Id);
-            return warns;
-        }
-    }
+		public async Task<UserWarns> GetWarns(ulong userId, bool fetch = false)
+		{
+			var warns = await GetWarns<UserWarns>(userId);
+			if (fetch)
+				warns.User = await m_clientConfig.Rest.GetUserAsync(warns.Id);
+			return warns;
+		}
+	}
 }
