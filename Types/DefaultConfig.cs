2b2133685d747845ea4068732d367ab8c73ecf93
Alexander <satan1c@ukr.net>
2023-05-20 13:43:38 +0300

    reformat

--- Types/DefaultConfig.cs
+++ Types/DefaultConfig.cs
@@ -1,24 +1,24 @@
 using Discord;
 using Discord.WebSocket;
+using SDC_Sharp.DiscordNet.Interfaces;
 
-namespace SDC_Sharp.DiscordNet.Types
+namespace SDC_Sharp.DiscordNet.Types;
+
+public sealed class DefaultConfig : IClientConfig
 {
-	public sealed class DefaultConfig : IClientConfig
+	private readonly DiscordSocketClient m_client;
+
+	public DefaultConfig(DiscordSocketClient client)
 	{
-		private readonly DiscordSocketClient m_client;
-
-		internal DefaultConfig(DiscordSocketClient client)
-		{
-			m_client = client;
-		}
-
-		internal DefaultConfig(IDiscordClient client)
-		{
-			m_client = client as DiscordSocketClient;
-		}
-
-		public uint GuildsCount => (uint)m_client.Guilds.Count;
-		public uint ShardsCount => 1;
-		public DiscordSocketRestClient Rest => m_client.Rest;
+		m_client = client;
+		Rest = new Rest(client.Rest);
 	}
+
+	public DefaultConfig(IDiscordClient client) : this(client as DiscordSocketClient)
+	{
+	}
+
+	public uint GuildsCount => (uint)m_client.Guilds.Count;
+	public uint ShardsCount => 1;
+	public IRest Rest { get; }
 }
